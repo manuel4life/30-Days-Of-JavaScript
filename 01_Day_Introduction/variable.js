3d288f75b63c83e5ef4924ab725c6b5b91bf1b9d
Nana Qofi <nanaqofi@Nanas-MacBook-Pro.local>
2024-04-27 21:27:15 +0000

    variable.js

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,19 @@
+// Declaring different variables of different data types
+let firstName = 'Emmanuel'  // First name of a person
+let lastName = 'Allotey' // LLast name of a person
+let country = 'Ghana' // Country
+let city = 'Accra' // Capital City
+let age = '121'  // age in years
+let isMarried  = true
+
+
+//Declaring variables with number values
+const gravity = 9.81 // earth gravity in m/s2
+const boilingPoint = 100 // water boiling point,temperature in oC
+const PI = 3.14 // geometrical constant
+
+
+// Variables can also be declarling in one line separated by comma
+let name = 'Emmanuel', // name of a person
+    job = 'software  developer'
+    live = 'Ghanaßßßß'
