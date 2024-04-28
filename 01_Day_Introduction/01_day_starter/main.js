373549b83c5fc6371b8939ed0d096a0b30f609da
Nana Qofi <nanaqofi@Nanas-MacBook-Pro.local>
2024-04-28 13:11:40 +0000

    JavaScript Files

---
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -0,0 +1,4 @@
+// the variables vaules can be accessed from other variables.js file
+console.log(firstName, lastName, country, city, age, isMarried)
+console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
+console.log(name, job,live)
